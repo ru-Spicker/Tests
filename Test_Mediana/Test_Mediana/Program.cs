99072a947c968912068a9794ff05942f65e9a9f4
Evgeny Ukolov <ru.spicker@gmail.com>
2018-01-15 12:39:41 +0400

    first commit

---
+++ Test_Mediana/Test_Mediana/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Mediana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String in_str = "";
+            int tmp;
+            int window;
+            List<int> signal = new List<int>();
+            List<int> m_signal = new List<int>();
+
+
+            //Ввод даных и проверка их корректности
+
+            Console.WriteLine("Введите массив (пустая строка - окончание ввода)");
+            do
+            {
+                Console.Write("Введите число: ");
+                in_str = Console.ReadLine();
+                if (int.TryParse(in_str, out tmp)) signal.Add(tmp);
+                else continue;
+            } while (in_str != "");
+
+            Console.WriteLine("\nМассив: ");
+            foreach (int e in signal) Console.Write("{0} ", e);
+
+            do
+            {
+                Console.WriteLine("\n\nВведите размер окна: ");
+                in_str = Console.ReadLine();
+            } while (!int.TryParse(in_str, out window));
+
+            int dx;
+            int x_out = window / 2;
+            if (window % 2 > 0) dx = (window / 2) + 1;
+            else dx = window / 2;
+
+            if ((signal.Count() < 1) || (window < 1) || (signal.Count() < window))
+            {
+                Console.WriteLine("Некорректные данные");
+                Console.ReadKey();
+                return;
+            }
+
+            int[] tmp_signal = new int[window];
+            int right_edge, art_signal_num, m;
+            int i;
+
+            //Часть данных дублируются, т.к. окно выходит за пределы массива в начале вычисления
+            for (i = 0; i < dx; i++)
+            {
+                right_edge = dx + i; //Расчет количества элеменотов в пределах массива
+                art_signal_num = window - right_edge; //Расчет количества искуственно добавленых элементов
+                signal.CopyTo(0, tmp_signal, 0, right_edge);
+                signal.CopyTo(0, tmp_signal, right_edge, art_signal_num);
+
+                if (Mediana(tmp_signal, out m)) m_signal.Add(m);
+            }
+
+            //Окно в пределах массива
+            for (i = i - x_out; i <= signal.Count() - window; i++)
+            {
+                signal.CopyTo(i, tmp_signal, 0, window);
+
+                if (Mediana(tmp_signal, out m)) m_signal.Add(m);
+            }
+            //Часть данных дублируются, т.к. окно выходит за пределы массива в конце вычисления
+            for (; i < signal.Count() - x_out; i++)
+            {
+                art_signal_num = (i + window) - signal.Count();
+                signal.CopyTo(i, tmp_signal, 0, window - art_signal_num);
+                signal.CopyTo(signal.Count() - art_signal_num, tmp_signal, window - art_signal_num, art_signal_num);
+
+                if (Mediana(tmp_signal, out m)) m_signal.Add(m);
+            }
+
+
+            Console.Write("\n\nРезультат: ");
+            foreach (int e in m_signal) Console.Write("{0} ", e);
+
+            Console.ReadKey();
+        }
+        static bool Mediana(int[] arr, out int mediana)
+        {
+            mediana = 0;
+            if (arr.Count() == 0) return false;
+            if (arr.Count() == 1)
+            {
+                mediana = arr[0];
+                return true;
+            }
+            List<int> l = arr.ToList();
+
+            l.Sort();
+
+            if (l.Count() % 2 == 0)
+                mediana = (int)Math.Round((float)(l[l.Count / 2] + l[(l.Count / 2) + 1]) / 2);
+            else mediana = l[l.Count / 2];
+            return true;
+        }
+    }
+}
---
+++ Test_Winzor/Test_Winzor/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Winzor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int[] a1 = new int[] { 2, 3, 17, 6, 1 };
+                int[] a2 = new int[] { 17, 38, 9, 63, 41, 15 };
+                int[] a3 = new int[] { 7, 82, 33, 33, 33, 1, 56 };
+
+                Console.Write("Winsor 1 = {0}\n", Winsorizing(a1, 2 / 2));
+                Console.Write("Winsor 2 = {0}\n", Winsorizing(a2, 4 / 2));
+                Console.Write("Winsor 3 = {0}\n", Winsorizing(a3, 2 / 2));
+
+                Console.ReadKey();
+            }
+        static float Winsorizing(int[] a, int m)
+        {
+            int i = 0;
+            int y = 0;
+            int sum = 0;
+            int max1 = a[0];
+            int min1 = a[0];
+            int max2 = a[0];
+            int min2 = a[0];
+            int tmp_min = a[0];
+            int tmp_max = a[0];
+
+            //Первый поиск первых пределов
+            foreach (int el in a)
+                if (max1 < el) max1 = el;
+
+            foreach (int el in a)
+                if (min1 > el) min1 = el;
+
+            //Гарантируем что вторые пределы не равны первым
+            max2 = min1;
+            min2 = max1;
+
+            //Цикл задает глубину винзоризации
+            for (y = 0; y < m; y++)
+            {
+                //Поиск вторых пределов
+                foreach (int el in a)
+                    if ((max2 < el) && (el < max1))
+                        max2 = el;
+
+                for (i = 0; i < a.Count(); i++)
+                    if (a[i] == max1)
+                        a[i] = max2;
+
+                //Замена пределов
+                foreach (int el in a)
+                    if ((min2 > el) && (el > min1))
+                        min2 = el;
+
+                for (i = 0; i < a.Count(); i++)
+                    if (a[i] == min1)
+                        a[i] = min2;
+
+                //Сохраняем пределы и гарантируем неравенство первых и вторых пределов
+                tmp_min = max1;
+                tmp_max = min1;
+                max1 = max2;
+                min1 = min2;
+                max2 = tmp_max;
+                min2 = tmp_min;
+
+            }
+            foreach (int el in a)
+                sum += el;
+            return (float)sum / a.Count();
+        }
+
+    }
+}
